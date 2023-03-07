ebc13ceb3971b90a92d9f5be429112daf8174230
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-03-07 17:56:22 +0000

    added favicon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Colton
+// Created on:  march 2023
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("do you like pizza?").innerHTML = "do you like pizza?"
+}
